0817bbd4be726654dc0e623f6a32e040784217e2
pasquierT <titouan.pasquier@etu.unicaen.fr>
2026-03-06 09:58:48 +0100

    Point généreux

--- src/modele/Point.java
+++ src/modele/Point.java
@@ -1,5 +1,3 @@
-package modele;
-
 public class Point{
 
 	private int x;
@@ -17,7 +15,23 @@ public class Point{
 	public int getY(){
 		return this.y;
 	}
+	
+	public void translation(int dx, int dy){
+		this.x = this.x + dx;
+		this.y = this.y + dy;
+	}
+	
+	public void homothetie(Point centre, int k){
+		this.x = centre.getX() + k * (this.x - centre.getX());
+		this.y = centre.getY() + k * (this.y - centre.getY());
+	}
+	
+	public void homothetie(int centreX, int centreY, int k){
+		this.x = centreX + k * (this.x - centreX);
+		this.y = centreY + k * (this.y - centreY);
+	}
 
+	public String toString(){
+		return "(" + this.x + "," + this.y + ")";
+	}
 }
-
-
