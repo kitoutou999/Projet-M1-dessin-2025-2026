b65f441e19bb29bb6a59bc705dd8aa2842c73292
Maxim Chepy <chepy221@C304L-134A29.campus.unicaen.fr>
2026-03-06 09:40:25 +0100

    Formes

---
+++ src/modele/Rectangle.java
@@ -0,0 +1,19 @@
+public class Rectangle implements Forme{
+
+	private Point debut;
+	private Point fin;
+	
+	public Rectangle(Point debut, Point fin){
+		this.debut = debut;
+		this.fin = fin;
+	}
+	
+	public Point getDebut(){
+		return this.debut;
+	}
+	
+	public Point getFin(){
+		return this.fin;
+	}
+
+}
