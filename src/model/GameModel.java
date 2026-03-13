9053013dd2d1f4a337c1a100b5dbcd74e9821e90
Marius Guillais <guillai211@C304L-134A30.campus.unicaen.fr>
2026-03-13 09:03:06 +0100

    calcul intersection entre formes

--- src/model/GameModel.java
+++ src/model/GameModel.java
@@ -43,4 +43,76 @@ public class GameModel extends Observable {
         redShapes.add(f);
         notifyObservers();
     }
+
+    public boolean collisionBetween(Shape shape1,Shape shape2){
+        int comp = 0;
+        Circle c1 = null;
+        Circle c2 = null;
+        Rectangle r1 = null;
+        Rectangle r2 = null;
+        if(shape1.getClass() == Circle.class){
+            if(shape2.getClass() == Circle.class){
+                c1 = (Circle) shape1;
+                c2 = (Circle) shape2;
+                return collisionCircleCircle(c1,c2);
+            }else if(shape2.getClass() == Rectangle.class){
+                c1 = (Circle) shape1;
+                r2 = (Rectangle) shape2;
+                return collisionRectangleCircle(r2,c1);
+            }
+        }else if(shape1.getClass() == Rectangle.class){
+            if(shape2.getClass() == Circle.class){
+                r1 = (Rectangle) shape1;
+                c2 = (Circle) shape2;
+                return collisionRectangleRectangle(r1,r2);
+            }else if(shape2.getClass() == Rectangle.class){
+                r1 = (Rectangle) shape1;
+                r2 = (Rectangle) shape2;
+                return collisionRectangleCircle(r1,c2);
+            }
+        }
+        return false;
+    }
+
+    public boolean collisionCircleCircle(Circle c1,Circle c2){
+        double distance = Math.sqrt(Math.pow(c2.getCenter().getX()-c1.getCenter().getX(),2)+Math.pow(c2.getCenter().getY()-c1.getCenter().getY(),2));
+        if(distance <= (c1.getRadius()+c2.getRadius())){
+            return true;
+        }return false;
+    }
+    
+    public boolean collisionRectangleCircle(Rectangle r,Circle c){
+        int weight = Math.abs(r.getEnd().getX() - r.getStart().getX());
+        int height = Math.abs(r.getStart().getY() - r.getEnd().getY());
+        int closestX = Math.max(r.getEnd().getX(), Math.min(c.getCenter().getX(), r.getEnd().getX() + weight));
+        int closestY = Math.max(r.getEnd().getY(), Math.min(c.getCenter().getY(), r.getEnd().getY() + height));
+        int dx = c.getCenter().getX() - closestX;
+        int dy = c.getCenter().getY() - closestY;
+        int dxxyy = dx*dx + dy*dy;
+
+        if(dxxyy < Math.pow(c.getRadius(),2)){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    public boolean collisionRectangleRectangle(Rectangle r1,Rectangle r2){
+        if (r1.getEnd().getX() < r2.getStart().getX() ||
+            r1.getStart().getX() > r2.getEnd().getX() ||
+            r1.getStart().getY() < r2.getEnd().getY() ||
+            r1.getEnd().getY() > r2.getStart().getY()) {
+                return false;
+            }
+        return true;
+    }
+
+    public boolean wellPlacedForm(Shape s1){
+        for(int i = 0;i<this.redShapes.size();i++){
+            if(this.collisionBetween(s1,this.redShapes.get(i))){
+                return false;
+            }
+        }
+        return true;
+    }
 }
