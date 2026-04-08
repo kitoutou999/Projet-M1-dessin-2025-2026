4b17fa5502dab5d9fd46cec6c612471b1b7267c3
David Tom <tomdavid090220003@gmail.com>
2026-04-08 02:39:39 +0200

    Update Javadoc

--- run.sh
+++ run.sh
@@ -2,13 +2,3 @@
 shopt -s globstar
 mkdir -p out
 javac -d out src/**/*.java && cp -r src/resources out/ 2>/dev/null && java -cp out Main
-#javadoc
-javadoc -d doc -sourcepath src \
-  src/Main.java \
-  src/model/*.java \
-  src/model/shapes/*.java \
-  src/view/*.java \
-  src/controller/*.java \
-  src/controller/commands/*.java \
-  src/controller/states/*.java \
-  src/model/strategy/*.java
