a89421a7a8e777e9cefdc8134f58a1f1a4e766e8
David Tom <tomdavid090220003@gmail.com>
2026-04-08 13:49:35 +0200

    fix version compilation

--- compile.sh
+++ compile.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 shopt -s globstar
-mkdir -p out
+mkdir -p out plugins plugins-src/darkmode/out
 javac -d out src/**/*.java
 cp -r src/resources out/ 2>/dev/null
+javac -cp out -d plugins-src/darkmode/out plugins-src/darkmode/**/*.java
+jar cfm plugins/darkmode.jar plugins-src/darkmode/MANIFEST.MF -C plugins-src/darkmode/out .
--- run.sh
+++ run.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 shopt -s globstar
-mkdir -p out
-javac -d out src/**/*.java && cp -r src/resources out/ 2>/dev/null && java -cp out Main
+mkdir -p out plugins plugins-src/darkmode/out
+javac -d out src/**/*.java
+cp -r src/resources out/ 2>/dev/null
+javac -cp out -d plugins-src/darkmode/out plugins-src/darkmode/**/*.java
+jar cfm plugins/darkmode.jar plugins-src/darkmode/MANIFEST.MF -C plugins-src/darkmode/out .
+java -cp out Main
